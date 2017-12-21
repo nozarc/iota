ea3379cf4e7f99632d951021fe581132aa676e24
nozarc <delyachmad@gmail.com>
2017-12-21 21:18:40 +0700

    begin to build edit analyze

--- application/controllers/Teacher.php
+++ application/controllers/Teacher.php
@@ -170,6 +170,141 @@ class Teacher extends CI_Controller
 						break;
 				}
 				break;
+			case 'edit':
+				if (empty($data_x)) {
+					redirect($sess_level.'/analyze/list/')
+				}
+				switch ($data_y) {
+					case 'step_one'://tambah nilai jika benar, salah, dan tidak dijawab
+						$data['score_scale']=$this->analyze->get_score_scale()->result();
+						$this->form_validation->set_rules('subject','Subject','required');
+						$this->form_validation->set_rules('test_type','Test Type','required');
+						$this->form_validation->set_rules('score_scale','Score Scale','required|numeric');
+						$this->form_validation->set_rules('min_score','Minimum Score','required|numeric'); 
+						$this->form_validation->set_rules('test_date','Test Date','alpha_dash');
+						$this->form_validation->set_rules('test_correction_date','Correction Date','alpha_dash');
+						$this->form_validation->set_rules('test_report_date','Test Report Date','alpha_dash');
+						$this->form_validation->set_rules('report_location','Report Location','required');
+						if ($this->form_validation->run()) {
+							$st1=$this->input->post(null,true);
+							$st1['teacher_id']=$_SESSION['sess_uid'];
+							if ($st1!=(!empty($st1_data)?$st1_data:null)) {
+								if (empty($st1_id)) {
+								 	$this->analyze->newanalyze($st1,null);
+								 	$data['lol']=$st1;
+								}
+								else{
+									$this->analyze->updateNew($st1,$st1_id);
+								}
+							}
+							$_SESSION['st1_id']=$this->analyze->get($st1)->id;
+							$_SESSION['st1_data']=$this->input->post(null,true);
+							redirect($sess_level.'/analyze/new/step_two');
+						}
+						$this->template->display('newanalyze_st1',$data);
+						break;
+					case 'step_two':
+						if (empty($st1_id)) {
+							redirect($sess_level.'/analyze/new/');
+						}
+						$answerskey=$this->input->post('answer_key',true);
+						$questions=$this->input->post('question',true);
+						$measured_capabilities=$this->input->post('measured_capability',true);
+						if (!empty($answerskey)) {
+							foreach ($answerskey as $k => $value) {
+								$this->form_validation->set_rules('answer_key['.$k.']','Answer Key No '.$k,'required|max_length[1]');
+							}
+						}
+					
+						if ($this->form_validation->run()) {
+							if(empty($st2_data)){
+								$this->analyze->ins_quiz(batch_build($this->input->post(null,true),$st1_id),'batch');
+								$_SESSION['st2_data']=$this->input->post(null,true);
+							}
+							else{
+								$dbQuests=$this->analyze->get_quiz($st1_id);
+								$quests=$dbQuests;
+								foreach ($quests as $k1 => $v1) {
+									unset($quests[$k1]['id']);
+								}
+								$inputQuests=batch_build($this->input->post(null,true),$st1_id);
+								$dqcount=count($dbQuests);
+								$iqcount=count($inputQuests);
+								if($quests!=$inputQuests){
+									foreach ($dbQuests as $dqkey => $dqval) {
+										foreach ($inputQuests as $iqkey => $iqval) {
+											if ($dqval['quiz_number']==$iqval['quiz_number']) {
+												$this->analyze->upd_quiz($iqval,$dqval['id']);
+											}
+										}
+									}
+									if ($iqcount>$dqcount) {
+										for ($a=$dqcount; $a < $iqcount; $a++) { 
+											$this->analyze->ins_quiz($inputQuests[$a]);
+										}
+									}
+								/*	elseif ($iqcount<$dqcount) {
+										for ($b=$iqcount; $b < $dqcount; $b++) { 
+											$data['lol'][0].=" |delete>".$dbQuests[$a]['id'].'_'.$dbQuests[$a]['id'].'=>'.$dbQuests[$a]['id'].' from '.count($inputQuests);
+										}
+									}
+								*/	
+								$_SESSION['st2_data']=$this->input->post(null,true);
+								}
+							}
+							redirect($sess_level.'/analyze/new/step_three');
+						}
+						$this->template->display('newanalyze_st2',$data);
+						break;
+					case 'step_three':
+						if (empty($st1_id)) {
+							redirect($sess_level.'/analyze/new/');
+						}
+						if (!empty($this->input->post('class',true))) {
+							$this->form_validation->set_rules('class','Class','numeric');
+							if ($this->form_validation->run()) {
+								$_SESSION['st3_data']['choosen_class']=$this->input->post('class',true);
+							}
+						}
+						if (!empty($this->input->post('answer',true))) {
+							foreach ($this->input->post('answer',true) as $ky => $vl) {
+								$this->form_validation->set_rules('answer['.$ky.']','Answer','alpha');
+							}
+							if ($this->form_validation->run()) {
+								if (empty($st3_data['answer'])) {
+									$this->analyze->ins_answer(batch_build($this->input->post(null,true)['answer'],$st1_id,'answer'),'batch');
+								}
+								else{
+									$inAns=batch_build($this->input->post(null,true)['answer'],$st1_id,'answer');
+									$dbAns=$this->analyze->get_answer($st1_id,'all');
+									$dbAnswer=$dbAns;
+									foreach ($dbAns as $k1 => $v1) {
+										unset($dbAns[$k1]['id']);
+									}
+									
+									if ($inAns!=$dbAns) {
+										$this->analyze->upd_answer($inAns);
+									}	
+								}
+								$_SESSION['st3_data']['answer']=$this->analyze->get_answer($st1_id,'allpeople');
+								$this->analyze->ins_score(score($this->analyze->get_answer($st1_id,'allpeople'),$st2_data,$this->analyze->show($st1_id,$sess_uid,'notdone')),'batch');
+								$this->analyze->newanalyze($st1_id,'done');
+								$id_analyze=$st1_id;
+								unset($_SESSION['st1_id']);
+								unset($_SESSION['st1_data']);
+								unset($_SESSION['st2_data']);
+								unset($_SESSION['st3_data']);
+								redirect($sess_level.'/analyze/list/');
+								redirect($sess_level.'/analyze/result/'.$id_analyze);
+							}
+						}
+					@	$data['students']=$this->schooldata->classes('getmember','student',$_SESSION['st3_data']['choosen_class']);
+						$data['classes']=$this->schooldata->classes('get_all');
+					@	$data['st3_data']=$_SESSION['st3_data'];
+						$this->template->display('newanalyze_st3',$data);
+						break;
+				}
+				break;
 			
 			case 'result':
 				if (!empty($data_x)) {
